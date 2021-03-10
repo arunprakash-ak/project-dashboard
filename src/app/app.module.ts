cd7b9ba2b63a7fb4bb5e3b098b97a25d0304f178
Arunprakash Ak <74054426+arunprakash-ak@users.noreply.github.com>
2021-03-10 14:42:46 +0530

    Bootstrap, User and Admin Component Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {AppRoutingModule} from './app-routing.module'
+import {AppRoutingModule} from './app-routing.module';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AdminComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
 
-  constructor() { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {
+    console.info("constructor");
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+        console.info("ngOnInit");
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
 
+    // convenience getter for easy access to form fields
+    get f() { console.info("get f") ; return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        console.info("User name and password :: "+this.f.username.value + " "  +this.f.password.value);
+        if("admin" === this.f.username.value ){
+        this.router.navigate(['/admin']);
+        } else if("user" === this.f.username.value ){
+                       this.router.navigate(['/user']);
+                       } else {
+                       this.router.navigate(['/login']);
+                       }
+                       this.submitted = false;
+    }
 }
